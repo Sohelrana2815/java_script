971a5fc22c5c56ac9d74afb63f6f0e73e7d5ff1a
Sohelrana2815 <sohel152302@gmail.com>
2024-11-23 08:42:00 +0600

    added a addition func

--- function.js
+++ function.js
@@ -11,5 +11,15 @@ function halfNumber(number) {
 const output = tenTimes(77);
 const cutHalf = halfNumber(100);
 
-console.log("Ten times:", output);
-console.log("Half Number:", cutHalf);
+// console.log("Ten times:", output);
+// console.log("Half Number:", cutHalf);
+
+// added
+
+function add(num1, num2) {
+  const sum = num1 + num2;
+  return sum;
+}
+
+const result = add(5, 54);
+console.log("Result of sum: ", result);
