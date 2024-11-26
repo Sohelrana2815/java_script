f4a46a709351cdbc8a28219d2371cb896213517e
Sohelrana2815 <sohel152302@gmail.com>
2024-11-26 09:50:56 +0600

    js problem solving

---
+++ OneFree Drinks.js
@@ -0,0 +1,13 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const burgerPrice = 500;
+if (burgerPrice > 500) {
+  console.log("Free Drinks");
+} else {
+  console.log("pay 30 tk for coke");
+}
--- TwoBMI_Calculator.js
+++ TwoBMI_Calculator.js
@@ -11,3 +11,19 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - Otherwise, you are obese.
 
 */
+
+const height = 1.6;
+const weight = 70;
+
+let BMI = weight / (height * height);
+
+
+if (BMI < 18.5) {
+  console.log(`Your BMI is: ${BMI} and you are underweight `);
+} else if (BMI >= 18.5 && BMI <= 24.9) {
+  console.log(`Your BMI is: ${BMI} and you are normal `);
+} else if (BMI >= 25 && BMI <= 29.9) {
+  console.log(`Your BMI is: ${BMI} and you are overweight`);
+} else {
+  console.log(`Your BMI is: ${BMI} and you are obese`);
+}
