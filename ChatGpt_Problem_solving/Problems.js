2dd4c4f6dd9102124e8d924e03bca347d3d20667
Sohelrana2815 <sohel152302@gmail.com>
2024-11-28 20:39:01 +0600

    my commit

--- ChatGpt_Problem_solving/Problems.js
+++ ChatGpt_Problem_solving/Problems.js
@@ -1,3 +1,13 @@
 // 1. Even or Odd
 // Write a program that takes a number as input and checks if it is even or odd. Print "Even" or "Odd" accordingly.
 
+function isEven(number) {
+  if (number % 2 !== 1) {
+    return "Even Number";
+  } else {
+    return "Odd Number";
+  }
+}
+const checkIsEven = isEven(81);
+
+console.log(checkIsEven);
