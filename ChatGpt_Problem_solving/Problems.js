8cdcf2f0113b9679bd324af965ac7f672e2c29d7
Sohelrana2815 <sohel152302@gmail.com>
2024-11-30 15:40:38 +0600

    my commit

--- ChatGpt_Problem_solving/Problems.js
+++ ChatGpt_Problem_solving/Problems.js
@@ -1,6 +1,33 @@
-// Reverse String
+// 1. Reverse String
 
 function reverseString(str) {
-  console.log(str);
+  const splitString = str.split("");
+  const reverseArray = splitString.reverse();
+  const joinSplitString = reverseArray.join();
+  return joinSplitString;
 }
-console.log(reverseString("Hello"));
+
+// console.log(reverseString("Sohel Rana"));
+
+// 2. Fizzbuzz
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 15 === 0) console.log("FizzBuzz");
+//   else if (i % 3 === 0) console.log("Fizz");
+//   else if (i % 5 === 0) console.log("Buzz");
+//   else console.log(i);
+// }
+
+// 3. Max number
+
+const array = [3, 5, 66, 48, 96, 13, 12, -6, 454];
+
+let maxNumber = [0];
+
+for (const element of array) {
+  if (element > maxNumber) {
+    maxNumber = element;
+  }
+}
+
+console.log(maxNumber);
