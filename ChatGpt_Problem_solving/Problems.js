35f42fcbd72ea1783a1004788ae6fdd4977b6532
Sohelrana2815 <sohel152302@gmail.com>
2024-11-30 13:39:04 +0600

    reverse string

--- ChatGpt_Problem_solving/Problems.js
+++ ChatGpt_Problem_solving/Problems.js
@@ -1,13 +1,6 @@
-// 1. Even or Odd
-// Write a program that takes a number as input and checks if it is even or odd. Print "Even" or "Odd" accordingly.
+// Reverse String
 
-function isEven(number) {
-  if (number % 2 !== 1) {
-    return "Even Number";
-  } else {
-    return "Odd Number";
-  }
+function reverseString(str) {
+  console.log(str);
 }
-const checkIsEven = isEven(81);
-
-console.log(checkIsEven);
+console.log(reverseString("Hello"));
