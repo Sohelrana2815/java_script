a29b90481506a8b84a7284b4df9d91adb0968388
Sohelrana2815 <sohel152302@gmail.com>
2025-01-26 21:27:48 +0600

    solved

---
+++ function.js
@@ -0,0 +1,9 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(a, b, c, d) {
+  const result = a * b * c * d;
+  return result;
+}
+
+console.log(multiply(5, 2, 3, 5));
