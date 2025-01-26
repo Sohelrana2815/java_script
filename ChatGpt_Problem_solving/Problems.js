9536640eac5bb9d0c684665178954e12ff3daad3
Sohelrana2815 <sohel152302@gmail.com>
2025-01-26 11:40:19 +0600

    problem solving

--- ChatGpt_Problem_solving/Problems.js
+++ ChatGpt_Problem_solving/Problems.js
@@ -1,30 +1,47 @@
 // 1. Reverse String
 
+const string = "Hello World!";
 function reverseString(str) {
   const splitString = str.split("");
   const reverseArray = splitString.reverse();
-  const joinSplitString = reverseArray.join();
+  const joinSplitString = reverseArray.join("");
   return joinSplitString;
 }
 
-// console.log(reverseString("Sohel Rana"));
+const result2 = reverseString(string);
+console.log(result2);
 
 // 2. Fizzbuzz
 
 // for (let i = 1; i <= 100; i++) {
 //   if (i % 15 === 0) console.log("FizzBuzz");
-//   else if (i % 3 === 0) console.log("Fizz");
-//   else if (i % 5 === 0) console.log("Buzz");
+//   else if (i % 3 === 0) console.log("Fizz", i);
+//   else if (i % 5 === 0) console.log("Buzz", i);
 //   else console.log(i);
 // }
 
 // 3. Max number
 
-const numbers = [100, 2, 3, 5, 86, 41];
-function findMaxNumber(arr) {
-  // Initialize a variable to store the maximum value
-  let max = arr[0];
+// const numbers = [100, 2, 3, 5, 86, 41];
+// function findMaxNumber(arr) {
+//   // Initialize a variable to store the maximum value
+//   let max = arr[0];
 
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// const result = findMaxNumber(numbers);
+// // console.log(result);
+
+const numbers = [100, 2, 3, 6, 81, 24];
+
+function findMaxNumber(arr) {
+  let max = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i];
@@ -32,6 +49,5 @@ function findMaxNumber(arr) {
   }
   return max;
 }
-
 const result = findMaxNumber(numbers);
 console.log(result);
