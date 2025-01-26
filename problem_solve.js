f8c493f7015e7644630c08c685400cd5bcf36533
Sohelrana2815 <sohel152302@gmail.com>
2025-01-26 18:35:53 +0600

    solve

--- problem_solve.js
+++ problem_solve.js
@@ -1,45 +1,10 @@
-// 1st Option
-const car = {
-  make: "Toyota",
-  model: "Corolla",
-  year: 2020,
+let myObject = {
+  name: "John Doe",
+  age: 25,
+  city: "Example City",
+  isStudent: true,
 };
 
-const addProp = (car.passengerCapacity = 5);
-
-// console.log(car);
-
-//2nd
-
-const updatedCar = {
-  ...car,
-  "passenger capacity": 5,
-};
-
-// console.log(updatedCar);
-
-// 3rd
-
-const student = {
-  name: "Hamim Sakep",
-  id: 5421,
-  physics: {
-    subject: "HSC Physics",
-    author: "Shahajahan Tapan",
-    marks: 30,
-  },
-};
-
-// const physicsMarks = student.physics.marks;
-
-// using nested indexing
-
-// const physicsMarks = student["physics"]["marks"];
-
-const physics = student.physics;
-const physicsMarks = physics.marks;
-
-console.log("Physics marks: ", physicsMarks);
-
-
-// 
+for (const [prop, val] of Object.entries(myObject)) {
+  console.log(`key: ${prop} | type: ${typeof val}`);
+}
