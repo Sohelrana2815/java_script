85b15fec0edac4c6d0b10447316b593e3ebb5340
Sohelrana2815 <sohel152302@gmail.com>
2025-01-27 09:59:51 +0600

    solved function related problems

--- function.js
+++ function.js
@@ -6,4 +6,26 @@ function multiply(a, b, c, d) {
   return result;
 }
 
-console.log(multiply(5, 2, 3, 5));
+// console.log(multiply(5, 2, 3, 5));
+
+// task 2
+
+function processNumber(number) {
+  if (number % 2 !== 0) {
+    return number * 2;
+  } else {
+    return number / 2;
+  }
+}
+
+const num1 = 9;
+
+const result1 = processNumber(num1);
+
+console.log(`Result for num1: ${result1}`);
+
+const num2 = 10;
+
+const result2 = processNumber(num2);
+
+console.log(`Result for num2: ${result2}`);
