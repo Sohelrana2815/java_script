e59c26565c61fdedabca0d322f74b841b16359ba
Sohelrana2815 <sohel152302@gmail.com>
2025-02-01 05:49:48 +0600

    solved 2 function problems

--- function.js
+++ function.js
@@ -1,5 +1,26 @@
 // task1
 
+function multiply(a, b, c, d) {
+  return a * b * c * d;
+}
+
+// console.log(multiply(5, 5, 2, 2));
+
+// task2
+
+function input(number) {
+  if (number % 2 !== 0) {
+    return number * 2;
+  } else {
+    return number / 2;
+  }
+}
+
+console.log(input(7));
+console.log(input(4));
+
+// task4
+
 function count_zero(binary_string) {
   let count = 0;
 
@@ -12,4 +33,6 @@ function count_zero(binary_string) {
   return count;
 }
 
-console.log(count_zero("101110"));
+// console.log(count_zero("101110"));
+// console.log(count_zero("0000101"));
+// console.log(count_zero("000000"));
