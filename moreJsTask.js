4f3a1d039c98601abe343cdb89c9cf3319e6d4b4
Sohelrana2815 <sohel152302@gmail.com>
2025-02-07 22:24:45 +0600

    solved

--- moreJsTask.js
+++ moreJsTask.js
@@ -1,3 +1,20 @@
+// task 3
+
+function calculateAge(people) {
+  const currentYear = new Date().getFullYear();
+
+  return people.map((person) => ({
+    name: person.name,
+    age: currentYear - person.birthYear,
+  }));
+}
+
+const people = [
+  { name: "Alice", birthYear: 2000 },
+  { name: "Bob", birthYear: 2005 },
+];
+
+console.log(calculateAge(people));
 // task 6 Find the Oldest Person
 
 function findOldestPerson(peopleObj) {
@@ -7,12 +24,22 @@ function findOldestPerson(peopleObj) {
   return oldestPerson;
 }
 
-const people = [
+const people1 = [
   { name: "Alice", age: 33 },
   { name: "Bob", age: 45 },
 ];
 
-console.log(findOldestPerson(people), "is the oldest person");
+console.log(findOldestPerson(people1), "is the oldest person");
+
+// task 7 Calculate Factorial
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * (n - 1);
+}
+console.log(factorial(5), "good");
 
 // task 9 Shopping Cart Total
 
@@ -34,3 +61,36 @@ const cart = [
 ];
 
 console.log(calculateTotal(cart, 20));
+
+// task 10 Find Duplicate Numbers
+
+function findDuplicates(numbers) {
+  const seen = new Set(); // Set to store unique numbers
+  const duplicates = new Set(); // Set to store duplicates numbers
+
+  for (const num of numbers) {
+    if (seen.has(num)) {
+      duplicates.add(num); // if number is already seen, add to duplicates
+    } else {
+      seen.add(num);
+    }
+  }
+  return [...duplicates];
+}
+
+console.log(findDuplicates([1, 2, 3, 4, 2, 5, 5]));
+
+function convertTemperature(value, unit) {
+  if (unit === "C") {
+    const celsius = ((value - 32) * 5) / 9;
+    return `${celsius}°F`;
+  } else if (unit === "F") {
+    const fahrenheit = (value * 9) / 5 + 32;
+    return `${fahrenheit}°F`;
+  } else {
+    ("Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit.");
+  }
+}
+
+console.log(convertTemperature(32, "C"));
+console.log(convertTemperature(45, "F"));
