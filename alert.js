f72dcab8ecb39e419c59bcc5ff965b09909e70a3
Sohelrana2815 <sohel152302@gmail.com>
2025-02-26 11:03:44 +0600

    successfully added comments and upvote and downvote

---
+++ alert.js
@@ -0,0 +1,5 @@
+// let condition = prompt("How are you?", "Write here...");
+// alert(`You are ${condition}`);
+
+let isBoss = confirm("Are you the boss?");
+alert(isBoss);
---
+++ type.js
@@ -0,0 +1,29 @@
+// let value = false;
+
+// // alert(typeof value); // boolean
+
+// // value = String(value);
+// // alert(typeof value); // string
+// // alert("60" / "5");
+// // console.log(typeof value);
+
+// // let str = "123";
+
+// // // alert(typeof str);
+
+// // let num = Number(str);
+
+// // // alert(typeof num);
+// // // console.log(num);
+
+// let age = "7" - "eight";
+
+// alert(age);
+
+// alert(Number(" 123 ")); // 123
+// alert(Number("123a")); // NaN
+// alert(Number(true)); // 1
+// alert(Number(false)); // 0
+// alert(Number(null)); // 0
+
+// alert(Number(undefined)); // NaN
